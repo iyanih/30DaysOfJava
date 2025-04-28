3a294c718b45a5b9f60adc4fa6ba6a27344f0098
iyanih <septiansaputra805@gmail.com>
2025-04-28 14:05:53 +0700

    Add files via upload

---
+++ newfile.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Halo, ini hari pertama gua belajar java! ");
+		System.out.println("=====================");
+		System.out.println("Nama       :  Septian Saputra");
+		System.out.println("Tools       :  Jvdroid");
+		System.out.println("Target      :  Kuasai Java Dalam 30 Hari!");
+		System.out.println("====================");
+	}
+}
